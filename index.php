7e0409a0b8245bb4eb075279bf4d12e210bbf328
801DevLabs <801DevLabs@gmail.com>
2019-02-23 21:00:51 -0700

    updated index.php, set username and password input filds as required

--- index.php
+++ index.php
@@ -89,11 +89,11 @@ if(!empty($feedback)) {
     <form action="index.php" method="POST">
         <div class="form-group">
             <label for="username">Username</label>
-            <input type="text" name="username" class="form-control <?php echo(!empty($username_err)) ? 'is-invalid' : ''; ?>" id="username" aria-describedby="emailHelp" placeholder="Enter username">
+            <input type="text" name="username" class="form-control <?php echo(!empty($username_err)) ? 'is-invalid' : ''; ?>" id="username" aria-describedby="emailHelp" placeholder="Enter username" required>
         </div>
         <div class="form-group">
             <label for="password">Password</label>
-            <input type="password" name="password" class="form-control <?php echo(!empty($password_err)) ? 'is-invalid' : ''; ?>" id="password" placeholder="Password">
+            <input type="password" name="password" class="form-control <?php echo(!empty($password_err)) ? 'is-invalid' : ''; ?>" id="password" placeholder="Password" required>
         </div>
         <button type="submit" name="submit" class="btn btn-primary">Login</button>
     </form>
